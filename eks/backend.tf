29b6e54e6866cb04c01397460c70e61db7476cdb
Sainikhilpothuraju <149396738+Sainikhilpothuraju@users.noreply.github.com>
2024-09-24 14:48:51 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "incred-bucket"
+    bucket         = "incred-bucket-1"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     //dynamodb_table = "Lock-Files"
